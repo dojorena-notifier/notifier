b03e0f0611847f87d5062667893eb3d79af433a4
stefan-rangelov-epam <stefan_rangelov@epam.com>
2020-12-17 15:04:29 +0200

    object equality fix and email implemenation change

--- notifier/src/main/java/com/epam/dojo/notifier/model/UserInfo.java
+++ notifier/src/main/java/com/epam/dojo/notifier/model/UserInfo.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -13,5 +13,18 @@ public class UserInfo {
     private long id;
     private String name;
     private String picture;
-    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserInfo)) return false;
+        UserInfo userInfo = (UserInfo) o;
+        return id == userInfo.id &&
+                name.equals(userInfo.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
